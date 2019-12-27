693a2031117306096dab6ae51b05504a059ae13a
rualeksandr <45438705+rualeksandr@users.noreply.github.com>
2019-12-27 11:41:36 +0500

    14. Объекты

--- script.js
+++ script.js
@@ -161,13 +161,45 @@
 
 // Пример 2 callback вызова 
 
-function learnJS(lang, callback) {
-  console.log("Я учу " + lang);
-  callback();
-}
+// function learnJS(lang, callback) {
+//   console.log("Я учу " + lang);
+//   callback();
+// }
 
-function done() {
-  console.log("Я прошел 3й урок!");
-}
+// function done() {
+//   console.log("Я прошел 3й урок!");
+// }
 
-learnJS("JavaScript", done);
+// learnJS("JavaScript", done);
+
+
+
+
+
+// ОБЪЕКТЫ
+
+// let obj = new Object();
+
+// let options = {
+//   width: 1024,
+//   height: 1024,
+//   name: "test"
+// };
+
+// console.log(options.name);
+
+// options.bool = false;
+// options.color = {
+//   border: "black",
+//   bg: "red"
+// };
+
+// delete options.bool;
+
+// console.log(options);
+
+// for (let key in options) {
+//   console.log('Свойство ' + key + ' имеет значение ' + options[key]);
+// }
+
+// console.log(Object.keys(options).length);
